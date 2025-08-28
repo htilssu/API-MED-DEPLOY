35ebef533245efd6fcd98ec8b68c2c49871e9fc9
htilssu <tolashuu@gmail.com>
2025-08-28 15:39:37 +0700

    remove unuse import

--- app/controller/med.py
+++ app/controller/med.py
@@ -1,58 +1,41 @@
-import cv2
-import numpy as np
-import time
-import torch
-import faiss
 import json
-from pathlib import Path
-from google.cloud import storage
-from transformers import CLIPProcessor, CLIPModel
-import google.generativeai as genai
-from transformers import pipeline
-from huggingface_hub import login
-import timm
-from PIL import Image
-from torchvision import transforms
 import logging
+import os
 # from transformers import Blip2Processor, Blip2ForConditionalGeneration
-import textwrap
 import re
-from sklearn.metrics.pairwise import cosine_similarity
-import requests
-import xml.etree.ElementTree as ET
-from typing import Optional, List, Dict, Tuple
-from io import BytesIO
-from fastapi import FastAPI, File, UploadFile, HTTPException
-from fastapi.responses import JSONResponse
-from pydantic import BaseModel
-from app.redis_client import redis_client, save_result_to_redis,get_result_by_key
-import hashlib
-from app.db.mongo import db
-from geopy.geocoders import Nominatim
 import time
-import io
+import xml.etree.ElementTree as ET
+from typing import Optional, List, Dict
 
-from app.pipeline.preprocess_image import preprocess_image
-from app.pipeline.AI_Agent.generate_description import generate_description_with_Gemini
-from app.pipeline.gsc.download_all_required_files import download_all_required_files
-from app.pipeline.anomaly import generate_anomaly_overlay,save_anomaly_outputs,embed_anomaly_heatmap
-from app.pipeline.embedding import embed_image_clip
-from app.pipeline.AI_Agent.different_question import generate_discriminative_questions
-from app.pipeline.AI_Agent.diagnose_group import generate_diagnosis_with_gemini
-from app.pipeline.AI_Agent.final_diagnose import select_final_diagnosis_with_llm
+import faiss
+import google.generativeai as genai
+import numpy as np
+import requests
+import timm
+import torch
+from fastapi import File, UploadFile, HTTPException
+from fastapi.responses import JSONResponse
+from google.cloud import storage
+from huggingface_hub import login
+from transformers import CLIPProcessor, CLIPModel
+
 from app.config.setting import setting
+from app.db.mongo import db
+from app.pipeline.AI_Agent.diagnose_group import generate_diagnosis_with_gemini
+from app.pipeline.AI_Agent.different_question import generate_discriminative_questions
+from app.pipeline.AI_Agent.final_diagnose import select_final_diagnosis_with_llm
+from app.pipeline.AI_Agent.generate_description import generate_description_with_Gemini
+from app.pipeline.anomaly import generate_anomaly_overlay, save_anomaly_outputs, embed_anomaly_heatmap
+from app.pipeline.embedding import embed_image_clip
+from app.pipeline.preprocess_image import preprocess_image
+from app.redis_client import redis_client, save_result_to_redis, get_result_by_key
 
 user_collection = db["user"]
-
-
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 login(token=setting.HUGGINGFACE_TOKEN)
 genai.configure(api_key=setting.GEMINI_API_KEY)
-
-
-app = FastAPI()
 
 # ---------------------- CONSTANTS ----------------------
 ROI_OUTPUT_DIR = "app/static/roi_outputs"
@@ -65,12 +48,7 @@ LOCAL_DATASET_PATH = "app/static/json/dataset.json"
 GCS_BUCKET_2 = "rag_3"
 LOCAL_SAVE_DIR_2 = "app/processed"
 
-
-
-
 GCS_KEY_PATH = storage.Client.from_service_account_json("app/iam-key.json")
-
-
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32").to(device)
@@ -101,9 +79,6 @@ def generate_disease_name(disease_name: str) -> str:
         return "Xảy ra lỗi trong quá trình dịch tên bệnh"
 
 
-
-
-
 def search_faiss_index(embedding: np.ndarray, index_path: str, label_path: str, top_k: int = 5):
     index = faiss.read_index(index_path)
     labels = np.load(label_path, allow_pickle=True)
@@ -111,8 +86,10 @@ def search_faiss_index(embedding: np.ndarray, index_path: str, label_path: str, 
     top_labels = [labels[idx] for idx in indices[0]]
     return list(zip(top_labels, distances[0]))
 
+
 def search_faiss_anomaly_index(embedding: np.ndarray, index_path: str, label_path: str, top_k: int = 5):
     return search_faiss_index(embedding, index_path, label_path, top_k)
+
 
 def normalize_group_name(group_name: str) -> str:
     """
@@ -126,9 +103,10 @@ def normalize_group_name(group_name: str) -> str:
     """
     group_name = group_name.lower()
     group_name = group_name.strip()
-    group_name = re.sub(r"\s+", "_", group_name)         
-    group_name = re.sub(r"[^a-z0-9_]", "", group_name)     
+    group_name = re.sub(r"\s+", "_", group_name)
+    group_name = re.sub(r"[^a-z0-9_]", "", group_name)
     return group_name
+
 
 def aggregate_combined_results(combined_results):
     score_dict = {}
@@ -151,9 +129,6 @@ def format_diagnosis_output(diagnosis):
     else:
         return str(diagnosis)
 
-    
-
-
 
 def detailed_group_analysis(image_vector: np.ndarray, anomaly_vector: np.ndarray, group_name: str, top_k: int = 5):
     print(f"\nSo sánh trong nhóm bệnh: {group_name}")
@@ -172,7 +147,7 @@ def detailed_group_analysis(image_vector: np.ndarray, anomaly_vector: np.ndarray
     )
     for label, score in full_group_results:
         similarity = 1 / (1 + score)
-        print(f"  → {label} (similarity: {similarity*100:.2f}%)")
+        print(f"  → {label} (similarity: {similarity * 100:.2f}%)")
 
     print(f"\nKết quả so khớp anomaly heatmap với {group_name}:")
     anomaly_group_results = search_faiss_index(
@@ -183,7 +158,7 @@ def detailed_group_analysis(image_vector: np.ndarray, anomaly_vector: np.ndarray
     )
     for label, score in anomaly_group_results:
         similarity = 1 / (1 + score)
-        print(f"  → {label} (similarity: {similarity*100:.2f}%)")
+        print(f"  → {label} (similarity: {similarity * 100:.2f}%)")
 
     # ========== GỘP NHÃN VÀ VOTING ==========
     print(f"\nGộp nhãn từ 2 pipeline (ảnh thường + anomaly) trong nhóm '{group_name}':")
@@ -206,8 +181,9 @@ def detailed_group_analysis(image_vector: np.ndarray, anomaly_vector: np.ndarray
     print("Tổng điểm similarity trong nhóm bệnh (%):")
     for label, percent in sorted_labels:
         print(f"  → {label}: {percent:.2f}%")
-    
+
     return sorted_labels
+
 
 def normalize_diagnosis(diagnosis: str) -> str:
     """
@@ -217,12 +193,10 @@ def normalize_diagnosis(diagnosis: str) -> str:
     return diagnosis.strip().lower().replace("-", "_").replace(" ", "_")
 
 
-
-
 async def start_diagnois(image: UploadFile = File(...), user_id: Optional[str] = None):
     try:
         start_time = time.time()
-        
+
         # --- B1: Kiểm tra input ---
         step_start = time.time()
         if not image:
@@ -237,7 +211,7 @@ async def start_diagnois(image: UploadFile = File(...), user_id: Optional[str] =
         step_start = time.time()
         image_data = await image.read()
         print(f"[TIME] Đọc ảnh: {time.time() - step_start:.3f} giây")
-        
+
         # --- B3: Sinh mô tả bằng Gemini ---
         step_start = time.time()
         description = generate_description_with_Gemini(image_data)
@@ -249,7 +223,7 @@ async def start_diagnois(image: UploadFile = File(...), user_id: Optional[str] =
         step_start = time.time()
         preprocessed_pil, preprocessed_np = preprocess_image(image_data)
         print(f"[TIME] Tiền xử lý ảnh: {time.time() - step_start:.3f} giây")
-        
+
         # --- B5: Tính embedding và tìm kiếm ban đầu ---
         step_start = time.time()
         test_vector = embed_image_clip(preprocessed_pil)
@@ -275,12 +249,14 @@ async def start_diagnois(image: UploadFile = File(...), user_id: Optional[str] =
             similarity = 1 / (1 + distance)
             label_scores_test[label] = label_scores_test.get(label, 0) + similarity
         total_similarity_test = sum(label_scores_test.values())
-        label_scores_percent_test = {label: (score / total_similarity_test) * 100 for label, score in label_scores_test.items()}
+        label_scores_percent_test = {label: (score / total_similarity_test) * 100 for label, score in
+                                     label_scores_test.items()}
         sorted_labels_test = sorted(label_scores_percent_test.items(), key=lambda x: x[1], reverse=True)
         print(f"[TIME] Xử lý kết quả ban đầu: {time.time() - step_start:.3f} giây")
 
         if sorted_labels_test[0][0] == "non-infectious diseases":
-            raise HTTPException(status_code=400, detail="Không thể chẩn đoán bệnh này vì hiện tại bệnh thuộc vào nhóm bệnh chưa hỗ trợ")
+            raise HTTPException(status_code=400,
+                                detail="Không thể chẩn đoán bệnh này vì hiện tại bệnh thuộc vào nhóm bệnh chưa hỗ trợ")
 
         # --- B7: Tìm kiếm full image ---
         step_start = time.time()
@@ -324,7 +300,7 @@ async def start_diagnois(image: UploadFile = File(...), user_id: Optional[str] =
         normalized_group_name = normalize_group_name(group_name_raw)
         diagnosis = generate_diagnosis_with_gemini(description, combined_results)
         print(f"[TIME] Chẩn đoán nhóm bệnh gemini: {time.time() - step_start:.3f} giây")
-        
+
         step_start = time.time()
         normalized_group_diagnosis = normalize_diagnosis(diagnosis)
         print(f"[TIME] Chẩn đoán nhóm bệnh: {time.time() - step_start:.3f} giây")
@@ -372,6 +348,7 @@ async def get_diagnosis_result(key: str):
         raise HTTPException(status_code=404, detail="Không tìm thấy kết quả")
     return result
 
+
 async def get_discriminative_questions(key: str):
     try:
         start_time = time.time()
@@ -413,8 +390,9 @@ async def get_discriminative_questions(key: str):
     except Exception as e:
         logger.exception("Lỗi trong quá trình lấy câu hỏi phân biệt")
         raise HTTPException(status_code=500, detail="Lỗi trong quá trình lấy câu hỏi phân biệt") from e
-    
-async def submit_discriminative_questions(user_answers:str,key:str):
+
+
+async def submit_discriminative_questions(user_answers: str, key: str):
     try:
         if not key:
             raise HTTPException(status_code=400, detail="Key không được để trống")
@@ -437,7 +415,7 @@ async def submit_discriminative_questions(user_answers:str,key:str):
             labels=disease_primary,
             questions=questions,
             answers=user_answers,
-            group_disease_name= normalized_group_name
+            group_disease_name=normalized_group_name
         )
         if not final_diagnosis:
             raise HTTPException(status_code=500, detail="Không thể đưa ra chẩn đoán cuối cùng")
@@ -450,7 +428,7 @@ async def submit_discriminative_questions(user_answers:str,key:str):
         logger.error(f"Lỗi HTTP: {e.detail}")
         raise e
 
-    
+
 def translate_disease_name(disease_name: str) -> str:
     prompt = f"""
     Bạn là một chuyên gia y tế, bạn có khả năng dịch tên bệnh từ tiếng việt sang tiếng Anh (Tên khoa học của bệnh đó).
@@ -471,7 +449,8 @@ def translate_disease_name(disease_name: str) -> str:
     except Exception as e:
         logger.error(f"Lỗi khi dịch tên bệnh: {e}")
         return "Xảy ra lỗi trong quá trình dịch tên bệnh"
-    
+
+
 def search_disease_in_json(file_path: str, disease_name: str) -> List[Dict]:
     try:
         with open(file_path, 'r', encoding='utf-8') as f:
@@ -488,7 +467,8 @@ def search_disease_in_json(file_path: str, disease_name: str) -> List[Dict]:
     except Exception as e:
         logger.error(f"Lỗi tìm kiếm trong JSON: {e}")
         return []
-    
+
+
 def search_medlineplus(ten_khoa_hoc: str) -> Optional[str]:
     if not ten_khoa_hoc or ten_khoa_hoc == "Không tìm thấy":
         logger.warning("Không có tên bệnh truyền vào cho MedlinePlus")
@@ -507,6 +487,8 @@ def search_medlineplus(ten_khoa_hoc: str) -> Optional[str]:
     except Exception as e:
         logger.error(f"Lỗi tìm kiếm MedlinePlus: {e}")
         return None
+
+
 def clean_text_json(data):
     if isinstance(data, dict):
         return {key: clean_text_json(value) for key, value in data.items()}
@@ -515,12 +497,14 @@ def clean_text_json(data):
     else:
         return clean_text(str(data))
 
+
 def clean_text(text: str) -> str:
     if not isinstance(text, str):
         return str(text)
     text = re.sub(r'[\\\n\r\t\*\]]', ' ', text)
     text = re.sub(r'\s+', ' ', text)
     return text.strip()
+
 
 def clean_xml_content(xml_content: bytes) -> str:
     try:
@@ -530,6 +514,7 @@ def clean_xml_content(xml_content: bytes) -> str:
     except ET.ParseError as e:
         logger.error(f"Lỗi phân tích XML: {e}")
         return ""
+
 
 def extract_medical_info(text: str) -> Dict:
     prompt = f"""
@@ -567,7 +552,8 @@ def extract_medical_info(text: str) -> Dict:
     except Exception as e:
         logger.error(f"Lỗi trích xuất thông tin y khoa: {e}")
         return {}
-    
+
+
 def download_from_gcs():
     storage_client = GCS_KEY_PATH
     bucket = storage_client.bucket(GCS_BUCKET)
@@ -579,7 +565,6 @@ def download_from_gcs():
         os.makedirs(os.path.dirname(local_path), exist_ok=True)
         blob.download_to_filename(local_path)
         logger.info(f"Tải về {gcs_path} to {local_path}")
-
 
 
 async def knowledge(disease_name: str):
@@ -595,7 +580,7 @@ async def knowledge(disease_name: str):
         start_time = time.time()
         medline_result = search_medlineplus(disease_name)
         if medline_result:
-            extracted_info = extract_medical_info(medline_result)   
+            extracted_info = extract_medical_info(medline_result)
             if extracted_info:
                 search_result = [extracted_info]
         if not search_result:
@@ -606,7 +591,8 @@ async def knowledge(disease_name: str):
     except Exception as e:
         logger.error(f"Lỗi khi tra cứu thông tin bệnh: {e}")
         raise HTTPException(status_code=500, detail=f"Lỗi khi tra cứu thông tin bệnh: {str(e)}")
-    
+
+
 async def knowledge_translate(disease_name: str):
     try:
         if not disease_name:
@@ -619,13 +605,14 @@ async def knowledge_translate(disease_name: str):
         print("Tra cứu thông tin bệnh...")
         medline_result = search_medlineplus(translated_name)
         if medline_result:
-            extracted_info = extract_medical_info(medline_result)   
+            extracted_info = extract_medical_info(medline_result)
             if extracted_info:
                 search_result = [extracted_info]
         if not search_result:
             raise HTTPException(status_code=404, detail=f"Không tìm thấy thông tin cho bệnh: {translated_name}")
         logger.info(f"Tra cứu thông tin bệnh: {translated_name}")
-        return JSONResponse(content={"translated_name": translated_name, "disease_info": search_result}, status_code=200)
+        return JSONResponse(content={"translated_name": translated_name, "disease_info": search_result},
+                            status_code=200)
     except HTTPException as e:
         logger.error(f"Lỗi HTTP: {e.detail}")
         raise e
